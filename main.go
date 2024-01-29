0a668eba71d8b1f1bcd03b6bb3249889a71cbfac
Ezequiel Zbogar <ezezbogar@gmail.com>
2024-01-29 01:24:32 -0300

    add swagger

--- main.go
+++ main.go
@@ -4,8 +4,17 @@ import (
 	"codequest/src/configs"
 	"codequest/src/routes"
 
+	_ "codequest/docs"
+
 	"github.com/gin-gonic/gin"
+
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+//	@title			CodeQuest API
+//	@version		1.0
+//	@description	Documentantion for CodeQuest API endpoints.
 
 func main() {
 	r := gin.Default()
@@ -14,6 +23,8 @@ func main() {
 
 	routes.UserRoute(r)
 	routes.JsExecutorRoute(r)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	_ = r.Run(":8080")
 }
